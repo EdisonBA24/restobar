c6b1fe780dbf112a2194e01029f1f8eaf828345c
EdisonBA24 <edisonbedoya2408@gmail.com>
2026-04-22 15:03:11 -0500

    implement conection supbase

--- config.py
+++ config.py
@@ -1,15 +1,30 @@
 import os
+import psycopg2
 
-class Config:
-    DB_DRIVER = "ODBC Driver 17 for SQL Server"
-    DB_SERVER = "LAPTOP-ULEKB954"
-    DB_DATABASE = "erp_restaurante"
+def get_connection():
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT"),
+        sslmode="require"
+    )
 
-    @staticmethod
-    def get_connection_string():
-        return (
-            f"DRIVER={{{Config.DB_DRIVER}}};"
-            f"SERVER={Config.DB_SERVER};"
-            f"DATABASE={Config.DB_DATABASE};"
-            "Trusted_Connection=yes;"
-        )
+
+##
+# import os
+
+#class Config:
+#    DB_DRIVER = "ODBC Driver 17 for SQL Server"
+#    DB_SERVER = "LAPTOP-ULEKB954"
+#    DB_DATABASE = "erp_restaurante"
+
+#    @staticmethod
+#    def get_connection_string():
+#        return (
+#            f"DRIVER={{{Config.DB_DRIVER}}};"
+#            f"SERVER={Config.DB_SERVER};"
+#            f"DATABASE={Config.DB_DATABASE};"
+#            "Trusted_Connection=yes;"
+#        )
